d6b5fe6dd13f77e7fd462edf3070739e997dfa3c
Lee Cookson <1561172+leecookson@users.noreply.github.com>
2025-06-18 14:49:46 -0400

    add circleci project IDs

--- aws/variables.tf
+++ aws/variables.tf
@@ -23,5 +23,13 @@ variable "default_tags" {
 variable "circleci_deploy_role_name" {
   type    = string
   default = "circle_ci_deploy"
+}
 
+variable "circleci_deploy_project_ids" {
+  type = list(string)
+  default = [
+    "fe1807b9-f7cf-4aef-9e4d-a89f3399c478",
+    "7d9508ea-a237-473b-b8b8-4d83c623d0e7",
+    "faa383a8-cc40-4ff1-8427-9d788ac3681b"
+  ]
 }
