5835be3fb9c653bf58fbf5b489c44281f8f4382e
Lee Cookson <1561172+leecookson@users.noreply.github.com>
2025-06-18 16:36:56 -0400

    tell backend to use "oidc-profile", won't work locally, but try in circleci

--- aws/provider.tf
+++ aws/provider.tf
@@ -8,5 +8,6 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1" # ACM certificates for CloudFront must be in us-east-1
+  region  = "us-east-1" # ACM certificates for CloudFront must be in us-east-1
+  profile = "oidc-profile"
 }
