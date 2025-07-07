0cdf0276bbd05dffd68d759579fb9cf8e06d2c63
Lee Cookson <1561172+leecookson@users.noreply.github.com>
2025-07-07 19:56:09 -0400

    update gcp login and permissions

--- gcp/oidc.tf
+++ gcp/oidc.tf
@@ -12,6 +12,13 @@ resource "google_project_iam_member" "circle_ci_deploy" {
   member  = "serviceAccount:${module.circleci_oidc.service_account_email}"
 }
 
+# Get the Workload Identity Pool Provider resource name for GitLab CI configuration
+output "circleci-workload-identity-provider" {
+  description = "The Workload Identity Provider resource name"
+  value       = module.circleci_oidc.workload_identity_pool_provider_id
+}
+
+# GitLab OIDC
 module "gitlab-wif" {
   source     = "Cyclenerd/wif-gitlab/google"
   version    = "~> 2.0.0"
@@ -41,3 +48,19 @@ module "gitlab-service-account" {
   account_id = google_service_account.gitlab.id
   repository = "cookson-group/*"
 }
+
+# Assign the 'roles/owner' role at the project level to the service account
+resource "google_project_iam_member" "gitlab" {
+  project = google_project.cookson_pro.project_id
+  role    = "roles/owner"
+  member  = "serviceAccount:${google_service_account.gitlab.email}"
+}
+
+resource "google_service_account_iam_binding" "gitlab_wif" {
+  service_account_id = google_service_account.gitlab.id
+  role               = "roles/owner"
+  members = [
+    "principalSet://iam.googleapis.com/${module.gitlab-wif.pool_name}/attribute.sub/*"
+  ]
+}
+
